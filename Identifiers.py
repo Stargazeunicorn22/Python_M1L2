20e43ea1c7eaf4cf500585d62a2c5eb9686435fa
stargazeunicorn22 <riapm.johari@gmail.com>
2026-09-23 19:44:20 -0700

    changes to second window

---
+++ Identifiers.py
@@ -0,0 +1,9 @@
+#Registration Info
+name= input("Good morning! What is your name? ")
+gender= input("What is your gender? ")
+age= input("What is your age? ")
+parent= input("What is your parent guardian's name? ")
+parent_email = input("What is your parent guardian's email? ")
+slot = input("What slot do you want? 7 am or 8 am: ")
+print("Here are your credentials: " + name, gender, age, parent, parent_email, slot )
+
